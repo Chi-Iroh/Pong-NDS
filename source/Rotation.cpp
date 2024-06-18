04db2c1fd797368a065b32fe5b29092c39a48193
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2024-06-18 09:30:53 +0200

    [REFACTO] Removed too small function

--- source/Rotation.cpp
+++ source/Rotation.cpp
@@ -1,13 +1,9 @@
 #include "Rotation.hpp"
 #include <nds.h>
 
-static inline int inverse_sign(int x) {
-    return -x;
-}
-
 std::pair<int, int> rotate(const std::pair<int, int>& move, Direction obstacleDirection) {
     if (obstacleDirection == Direction::Horizontal) {
-        return { move.first, inverse_sign(move.second) };
+        return { move.first, -move.second };
     }
-    return { inverse_sign(move.first), move.second };
+    return { -move.first, move.second };
 }
